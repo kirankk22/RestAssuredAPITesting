fb7b88860d841a644c793df544a5a20acf319bdb
kirankk22 <kirankumar2221974@gmail.com>
2025-03-24 18:17:23 +0530

    added code to POST call way-1 JSONAsString

--- src/test/java/in/at/tests/RestAssuredPOSTRequest.java
+++ src/test/java/in/at/tests/RestAssuredPOSTRequest.java
@@ -1,5 +1,32 @@
 package in.at.tests;
 
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 public class RestAssuredPOSTRequest {
+	@Test
+	public void addProductUsingJSONAsString() {
+		
+		//way-1
+		String jsonRequestBodyAsString = "{ "
+				+ "\"title\" : \"Laptop\","
+				+ "\"price\" : \"100\","
+				+ "\"description\" : \"Best Laptop\","
+				+ "\"category\" : \"electronics\""
+				+ "}";
+		
+		RestAssured.baseURI="https://fakestoreapi.com/products";
+		RequestSpecification requestSpec = RestAssured.given();
+		requestSpec.contentType(ContentType.JSON);
+		requestSpec.body(jsonRequestBodyAsString);
+		
+		Response response = requestSpec.post();
+		System.out.println(response.getBody().asPrettyString());
+				
+	}
 
 }
