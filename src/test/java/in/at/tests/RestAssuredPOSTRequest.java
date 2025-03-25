b296be0a66044fe3cf8623a8089d06f109a3cee1
kirankk22 <kirankumar2221974@gmail.com>
2025-03-25 13:44:46 +0530

    different types of request body for post method added

--- src/test/java/in/at/tests/RestAssuredPOSTRequest.java
+++ src/test/java/in/at/tests/RestAssuredPOSTRequest.java
@@ -1,6 +1,10 @@
 package in.at.tests;
 
+import org.json.JSONObject;
 import org.testng.annotations.Test;
+
+import com.google.gson.JsonObject;
+
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
@@ -8,10 +12,15 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import testdata.POJOProductRequest;
+
 import static io.restassured.RestAssured.*;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.naming.ContextNotEmptyException;
 
 public class RestAssuredPOSTRequest {
 	@Test
@@ -46,7 +55,7 @@ public class RestAssuredPOSTRequest {
 	
 	@Test
 	public void addProductUsingMap() {
-		// way-1
+		
 		// add gson dependency in pom.xml
 		
 		Map<String, String> jsonRequestBodyAsMap = new HashMap();
@@ -55,14 +64,143 @@ public class RestAssuredPOSTRequest {
 		jsonRequestBodyAsMap.put("description", "Best Laptop");
 		jsonRequestBodyAsMap.put("category", "electronics");
 		
-		RestAssured.baseURI = "https://fakestoreapi.com/products";
-		RequestSpecification requestSpec = RestAssured.given();
-		requestSpec.contentType(ContentType.JSON);
-		requestSpec.body(jsonRequestBodyAsMap);
+		// way-1
+//		RestAssured.baseURI = "https://fakestoreapi.com/products";
+//		RequestSpecification requestSpec = RestAssured.given();
+//		requestSpec.contentType(ContentType.JSON);
+//		requestSpec.body(jsonRequestBodyAsMap);
+//		
+//		Response response = requestSpec.post();
+//		System.out.println(response.getBody().asPrettyString());
 		
-		Response response = requestSpec.post();
-		System.out.println(response.getBody().asPrettyString());
+		// way-2
+		
+		given()
+			.contentType(ContentType.JSON)
+			.body(jsonRequestBodyAsMap)
+		.when()
+			.post("https://fakestoreapi.com/products")
+		.then()
+			.log().body();
+		
 		
 	}
+	
+	@Test
+	public void addProductUsingJSONObject() {
+		
+		// add json dependency from org.json
+		JSONObject jsonRequestBody = new JSONObject();
+		jsonRequestBody.put("title", "Laptop");
+		jsonRequestBody.put("price", "200");
+		jsonRequestBody.put("description", "Best Laptop");
+		jsonRequestBody.put("category", "electronics");
+		
+		// way-1
+//		RestAssured.baseURI = "https://fakestoreapi.com/products";
+//		RequestSpecification requestSpec = RestAssured.given();
+//		requestSpec.contentType(ContentType.JSON);
+//		requestSpec.body(jsonRequestBody.toString());
+//		
+//		Response response = requestSpec.post();
+//		System.out.println(response.getBody().asPrettyString());
+		
+		//way-2
+		given()
+			.contentType(ContentType.JSON)
+			.body(jsonRequestBody.toString())
+		.when()
+			.post("https://fakestoreapi.com/products")
+		.then()
+			.log().body();
+	}
+	
+	@Test
+	public void addProductsUsingPOJOClass() {
+		POJOProductRequest pojoRequest = new POJOProductRequest();
+		pojoRequest.setTitle("Laptop");
+		pojoRequest.setPrice("300");
+		pojoRequest.setDescription("Best Laptop");
+		pojoRequest.setCategory("electronics");
+		
+		//way-1
+//		RestAssured.baseURI = "https://fakestoreapi.com/products";
+//		RequestSpecification requestSpec = RestAssured.given();
+//		requestSpec.contentType(ContentType.JSON);
+//		requestSpec.body(pojoRequest);
+//		
+//		Response response = requestSpec.post();
+//		System.out.println(response.getBody().asPrettyString());
+		
+		//way-2
+		given()
+			.contentType(ContentType.JSON)
+			.body(pojoRequest)
+		.when()
+			.post("https://fakestoreapi.com/products")
+		.then()
+			.log().body();
+		
+	}
+	
+	@Test
+	public void addProductUsingExternalJSONFile() {
+		File requestJSONFile = new File("./product.json");
+		
+		// way-1
+//		RestAssured.baseURI = "https://fakestoreapi.com/products";
+//		RequestSpecification requestSpec = RestAssured.given();
+//		requestSpec.contentType(ContentType.JSON);
+//		requestSpec.body(requestJSONFile);
+//		
+//		Response response = requestSpec.post();
+//		System.out.println(response.getBody().asPrettyString());
+		
+		//way-2
+		given()
+			.contentType(ContentType.JSON)
+			.body(requestJSONFile)
+		.when()
+			.post("https://fakestoreapi.com/products")
+		.then()
+			.log().body();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
---
+++ src/test/java/testdata/POJOProductRequest.java
@@ -0,0 +1,33 @@
+package testdata;
+
+public class POJOProductRequest {
+	private String title;
+	private String price;
+	private String description;
+	private String category;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+}
