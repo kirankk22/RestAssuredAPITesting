64db2b0a94f45dcd83d2b5032f961929adde8283
kirankk22 <kirankumar2221974@gmail.com>
2025-03-25 10:39:34 +0530

    added a code for POST request using Map

--- src/test/java/in/at/tests/RestAssuredPOSTRequest.java
+++ src/test/java/in/at/tests/RestAssuredPOSTRequest.java
@@ -8,6 +8,10 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RestAssuredPOSTRequest {
 	@Test
@@ -21,14 +25,44 @@ public class RestAssuredPOSTRequest {
 				+ "\"category\" : \"electronics\""
 				+ "}";
 		
-		RestAssured.baseURI="https://fakestoreapi.com/products";
+//		RestAssured.baseURI="https://fakestoreapi.com/products";
+//		RequestSpecification requestSpec = RestAssured.given();
+//		requestSpec.contentType(ContentType.JSON);
+//		requestSpec.body(jsonRequestBodyAsString);
+//		
+//		Response response = requestSpec.post();
+//		System.out.println(response.getBody().asPrettyString());
+		
+		//way-2
+		given()
+			.contentType(ContentType.JSON)
+			.body(jsonRequestBodyAsString)
+		.when()
+			.post("https://fakestoreapi.com/products")
+		.then()
+			.log().body();
+					
+	}
+	
+	@Test
+	public void addProductUsingMap() {
+		// way-1
+		// add gson dependency in pom.xml
+		
+		Map<String, String> jsonRequestBodyAsMap = new HashMap();
+		jsonRequestBodyAsMap.put("title", "Laptop");
+		jsonRequestBodyAsMap.put("price", "150");
+		jsonRequestBodyAsMap.put("description", "Best Laptop");
+		jsonRequestBodyAsMap.put("category", "electronics");
+		
+		RestAssured.baseURI = "https://fakestoreapi.com/products";
 		RequestSpecification requestSpec = RestAssured.given();
 		requestSpec.contentType(ContentType.JSON);
-		requestSpec.body(jsonRequestBodyAsString);
+		requestSpec.body(jsonRequestBodyAsMap);
 		
 		Response response = requestSpec.post();
 		System.out.println(response.getBody().asPrettyString());
-				
+		
 	}
 
 }
